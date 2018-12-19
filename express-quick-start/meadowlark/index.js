f142922354ce305ce765d639852cdecb608632e6
Fu Wei <softbaddog@gmail.com>
2018-12-19 20:49:34 +0800

    QA

--- express-quick-start/meadowlark/index.js
+++ express-quick-start/meadowlark/index.js
@@ -15,32 +15,38 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(function (req, res, next) {
+  res.locals.showTests = app.get('env') != 'production' &&
+    req.query.test === '1';
+  next();
+});
+
 app.get('/', function (req, res) {
-  // res.type('text/plain');
-  // res.send('Meadowlark Travel');
   res.render('home');
 });
 
 app.get('/about', function (req, res) {
-  // res.type('text/plain');
-  // res.send('About Meadowlark Travel');
   res.render('about', {
-    fortune: fortune.getFortune()
+    fortune: fortune.getFortune(),
+    pageTestScript: '/qa/tests-about.js'
   });
 });
 
+app.get('/tours/hood-river', function (req, res) {
+  res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function (req, res) {
+  res.render('tours/request-group-rate');
+});
+
 app.use(function (req, res) {
-  // res.type('text/plain');
   res.status(404);
-  // res.send('404 - Not Found');
   res.render('404');
 });
 
 app.use(function (req, res) {
   console.error(err.stack);
-  // res.type('text/plain');
   res.status(500);
-  // res.send('500 - Server Error');
   res.render('500');
 });
 
--- express-quick-start/meadowlark/libs/fortune.js
+++ express-quick-start/meadowlark/libs/fortune.js
@@ -5,6 +5,7 @@ var fortuneCookies = [
   "You will have a pleasant surprise.",
   "Whenever possible, keep it simple.",
 ];
+
 exports.getFortune = function () {
   var idx = Math.floor(Math.random() * fortuneCookies.length);
   return fortuneCookies[idx];
